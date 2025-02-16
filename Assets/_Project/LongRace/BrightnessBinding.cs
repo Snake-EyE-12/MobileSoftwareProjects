af43144087758518af30495aa0fc79fe5cf1a80a
Snake-EyE-12 <chase.guymon@q.com>
2025-02-16 12:49:48 -0700

    Prefab Fixes

--- Assets/_Project/LongRace/BrightnessBinding.cs
+++ Assets/_Project/LongRace/BrightnessBinding.cs
@@ -16,7 +16,5 @@ public class BrightnessBinding : MonoBehaviour
     {
         float newAlpha = alpha * (1 - SettingsDataBinding.BrightnessValue);
         panel.color = new Color(panel.color.r, panel.color.g, panel.color.b, newAlpha);
-        Debug.Log("NEW ALPHA: " + newAlpha);
-        Debug.Log("BV: " + SettingsDataBinding.BrightnessValue);
     }
 }
